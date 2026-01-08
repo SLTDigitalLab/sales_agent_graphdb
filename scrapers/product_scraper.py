930c40b24144c4b1d72ae2a7d4a73e353660afb5
Madhara <madhara.emd@gmail.com>
2026-01-08 22:06:05 +0530

    Fix selenium docker crash

--- scrapers/product_scraper.py
+++ scrapers/product_scraper.py
@@ -17,6 +17,8 @@ def setup_driver():
     """Setup invisible Chrome browser"""
     chrome_options = Options()
     chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--log-level=3")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--window-size=1920,1080")
--- scrapers/website_scraper.py
+++ scrapers/website_scraper.py
@@ -25,8 +25,8 @@ class WebsiteScraper:
     def setup_driver(self):
         chrome_options = Options()
         chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--no-sandbox")             # <--- You were missing this!
-        chrome_options.add_argument("--disable-dev-shm-usage")  # <--- You were missing this!
+        chrome_options.add_argument("--no-sandbox")            
+        chrome_options.add_argument("--disable-dev-shm-usage") 
         chrome_options.add_argument("--log-level=3")
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--window-size=1920,1080")
